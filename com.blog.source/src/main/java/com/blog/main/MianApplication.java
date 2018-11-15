3e9132314805201e1c7b356b9ed46c65d9fa4929
mouyun <1402704730@qq.com>
2018-11-15 14:24:13 +0800

    com.blog.source

---
+++ com.blog.source/src/main/java/com/blog/main/MianApplication.java
@@ -0,0 +1,12 @@
+package com.blog.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MianApplication.class, args);
+    }
+}
